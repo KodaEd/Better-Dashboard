0beb022282d273ef16114563fbef84871370b1e4
KodaEd <edrickkoda@gmail.com>
2025-10-29 00:21:16 +1100

    implemented it properly with getter and setters

--- src/pages/settings.tsx
+++ src/pages/settings.tsx
@@ -1,28 +1,52 @@
-import { Button } from "@/components/ui/button";
 import { PasswordInput } from "@/components/ui/password-input";
-import { H1, H3 } from "@/components/ui/typography";
+import { Spinner } from "@/components/ui/spinner";
+import { H3 } from "@/components/ui/typography";
 import { load } from "@tauri-apps/plugin-store";
-import { useCallback, useState } from "react";
+import { Suspense, useState } from "react";
+import useSWR from "swr";
+
+const fetchApiKey = async () => {
+  const store = await load("settings.json");
+  const apiKey = (await store.get("apiKey")) || "";
+  return apiKey as string;
+};
+
+function ApiKeyInput() {
+  const { data: apiKey, mutate } = useSWR("apiKey", fetchApiKey, {
+    suspense: true,
+  });
+
+  const [value, setValue] = useState(apiKey || "");
+
+  const handleBlur = async (event: React.FocusEvent<HTMLInputElement>) => {
+    const newValue = event.target.value;
+    const store = await load("settings.json");
+    await store.set("apiKey", newValue);
+    await mutate();
+  };
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setValue(event.target.value);
+  };
+
+  return (
+    <PasswordInput
+      value={value}
+      onChange={handleChange}
+      onBlur={handleBlur}
+      placeholder="Enter your API key"
+    />
+  );
+}
 
 export default function Settings() {
-  const saveApiKey = async (event: React.ChangeEvent<HTMLInputElement>) => {
-    const apiKey = event.target.value;
-    const store = await load("settings.json");
-    store.set("apiKey", apiKey);
-  };
-
-  const getApiKey = async () => {
-    const store = await load("settings.json");
-    const apiKey = await store.get("apiKey") || "";
-    console.log(apiKey);
-  };
-
   return (
     <div>
       <div className="flex flex-col gap-2 w-1/2">
         <H3>Api Key:</H3>
-        <PasswordInput onBlur={saveApiKey} placeholder="Enter your password" />
-        <Button onClick={getApiKey}>Log</Button>
+        <Suspense fallback={<Spinner className="size-6" />}>
+          <ApiKeyInput />
+        </Suspense>
       </div>
     </div>
   );
