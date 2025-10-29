bf41b99c9d7a0f2e45657d67248ce32cfafb5fa4
KodaEd <edrickkoda@gmail.com>
2025-10-29 21:55:05 +1100

    added home button

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,15 @@
 import { Button } from "./ui/button";
 import { FaBitbucket } from "react-icons/fa";
 import { SiJira } from "react-icons/si";
-import { IoSettingsSharp } from "react-icons/io5";
+import { IoSettingsSharp, IoHome } from "react-icons/io5";
 import { useLocation } from "wouter";
 
 export default function Header() {
   const [_location, navigate] = useLocation();
 
+  const handleHomeClick = () => {
+    navigate("/");
+  };
   const handleBitbucketClick = () => {
     console.log("Bitbucket clicked");
   };
@@ -19,6 +22,9 @@ export default function Header() {
   return (
     <div className="flex items-center justify-between">
       <div className="flex items-center gap-2">
+        <Button variant="ghost" onClick={handleHomeClick}>
+          <IoHome />
+        </Button>
         <Button variant="ghost" onClick={handleBitbucketClick}>
           <FaBitbucket />
         </Button>
